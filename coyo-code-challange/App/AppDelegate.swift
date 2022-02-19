bd12a09e2d79a88f1c9b28617f878d6f9f447c40
Burak Nurçiçek <b.nurcicek@papara.com>
2022-02-19 20:45:19 +0300

    base view controller deleted

--- coyo-code-challange/App/AppDelegate.swift
+++ coyo-code-challange/App/AppDelegate.swift
@@ -9,14 +9,16 @@ import UIKit
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
+    
     var window: UIWindow?
-
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        window = UIWindow(frame: UIScreen.main.bounds)
         let viewModel = HomeViewModel()
-        window?.rootViewController = HomeViewController()
-        window?.makeKeyAndVisible()
+        let rootViewController = HomeViewController(viewModel: viewModel)
+        let navigationController = UINavigationController(rootViewController: rootViewController)
+        self.window = UIWindow(frame: UIScreen.main.bounds)
+        self.window?.rootViewController = navigationController
+        self.window?.makeKeyAndVisible()
         return true
     }
 }
