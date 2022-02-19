17cc1fc1d7eacfd998a00c68b3b2945ac56c4870
Burak Nurçiçek <b.nurcicek@papara.com>
2022-02-19 15:52:09 +0300

    info.plist fix

--- coyo-code-challange/Sceenes/Main/ViewController/HomeViewController.swift
+++ coyo-code-challange/Sceenes/Main/ViewController/HomeViewController.swift
@@ -12,7 +12,7 @@ class HomeViewController: BaseViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .red
     }
     
 
